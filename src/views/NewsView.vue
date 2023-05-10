ccbd1358587603d8f7402f22c95714cfdbaab774
comt.mix@gmail.com <comt.mix@gmail.com>
2023-05-11 01:37:05 +0900

    Feat: axios로 data 불러오기 구현

---
+++ src/views/NewsView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div v-for="data in news" :key="data.id">{{ data.title }}</div>
+  </div>
+</template>
+
+<script>
+import { fetchNewsList } from "../api/index.js";
+
+export default {
+  data() {
+    return {
+      news: [],
+    };
+  },
+  created() {
+    fetchNewsList()
+      .then((res) => (this.news = res.data))
+      .catch((err) => console.log(err));
+  },
+};
+</script>
+
+<style></style>
