e54e51be8ae2d25ad7f2e3afa36d92e734d3a833
comt-mix <comt.mix@gmail.com>
2023-05-12 14:15:37 +0900

    Feat: list 변경

--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
-    <p v-for="item in this.$store.state.news" :key="item.id">
-      <a :href="item.url">{{ item.title }}</a>
-      <small
-        >{{ item.time_ago }} by
-        <router-link :to="`/user/${item.user}`">{{ item.user }}</router-link>
-      </small>
-    </p>
+    <ul class="new-list">
+      <li v-for="item in this.$store.state.news" :key="item.id" class="post">
+        <div class="points">
+          {{ item.points}}
+        </div>
+        <a :href="item.url">{{ item.title }}</a>
+        <small
+          >{{ item.time_ago }} by
+          <router-link :to="`/user/${item.user}`">{{ item.user }}</router-link>
+        </small>
+      </li>
+    </ul>
   </div>
 </template>
 
@@ -23,4 +28,23 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.new-list {
+  margin: 0;
+  padding: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b883;
+}
+</style>
